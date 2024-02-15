b275235504f9fbfe46b990ff5b6cf4f4ecbfa968
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-02-15 10:16:51 +0300

    testing few changes

--- Testing Pages/freeCodeCamp.js
+++ Testing Pages/freeCodeCamp.js
@@ -1,87 +1,87 @@
 // Setup
 const recordCollection = {
-    2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
-    },
-    2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
-    },
-    1245: {
-      artist: 'Robert Palmer',
-      tracks: []
-    },
-    5439: {
-      albumTitle: 'ABBA Gold'
-    }
-  };
-  
-  // Only change code below this line
-  function updateRecords(records, id, prop, value) {
-    if (value === "") {
-      delete records[id][prop];
-    } else if (prop !== "tracks" && value !== "") {
-      records[id][prop] = value;
-    } else if (prop === "tracks" && value !== "") {
-      if (records[id].hasOwnProperty("tracks") === false) {
-        records[id][prop] = [];
-      }
-      records[id][prop].push(value);
-    }
-    return records;
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
   }
-  
-  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+};
 
-  console.log(recordCollection);
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (value === "") {
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "") {
+    if (records[id].hasOwnProperty("tracks") === false) {
+      records[id][prop] = [];
+    }
+    records[id][prop].push(value);
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+console.log(recordCollection);
 
 
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  // Setup
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Setup
 const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
-  
-  function lookUpProfile(name, prop) {
-    // Only change code below this line
-    for (let i = 0; i < contacts.length; i++){
-      if (name === contacts[i].firstName){
-        if (contacts[i].hasOwnProperty(prop)){
-          return contacts[i][prop];
-        }else {
-          return "No such property";
-        }
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  for (let i = 0; i < contacts.length; i++) {
+    if (name === contacts[i].firstName) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
       }
     }
-    return "No such contact";
-    // Only change code above this line
   }
-  
-  lookUpProfile("Akira", "likes");
+  return "No such contact";
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
