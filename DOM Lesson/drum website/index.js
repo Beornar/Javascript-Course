2460a746b004bf147d1183a53f7fe951c8ff58a2
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-03-03 13:24:33 +0300

    drum set

--- DOM Lesson/drum website/index.js
+++ DOM Lesson/drum website/index.js
@@ -1,7 +1,52 @@
-function handleClick() {
-    alert("Clicked!");
+// function handlePlay() {
+//     let audio = new Audio("./sounds/crash.mp3");
+//     audio.play();
+// }
+
+function playSound() {
+    let buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+        case "w":
+            let tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+            case "a":
+            let tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+            case "s":
+            let tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+            case "d":
+            let tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+            case "j":
+            let snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+
+            case "k":
+            let kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+            case "l":
+            let crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+
+        default:
+            break;
+    }
 }
 
 for (let i = 0; i < document.querySelectorAll("button").length; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+    document.querySelectorAll("button")[i].addEventListener("click", playSound);
 }
