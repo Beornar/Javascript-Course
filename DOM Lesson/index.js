3a4b4f08bb0586dc877fa5c30b34ef21a7f7434a
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-03-03 11:56:03 +0300

    dom update

--- DOM Lesson/index.js
+++ DOM Lesson/index.js
@@ -7,3 +7,11 @@ document.querySelector("li a").innerHTML = "Google It!";
 document.querySelectorAll("#list .item")[1].style.color = "gray";
 document.querySelector("h1").style.fontSize = "5rem";
 document.querySelector(".btn").style.backgroundColor = "red";
+
+document.querySelector("button").classList.add("invisible");
+document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("h1").textContent = "Hello";
+document.querySelector("h1").innerHTML = "<em> Hello </em>";
+
+document.querySelector("a").setAttribute("href", "https://www.linkedin.com");
