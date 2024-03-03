988bb5202092623e4f2d6a0751f0d736f4031458
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-03-03 10:53:57 +0300

    query selector

--- DOM Lesson/index.js
+++ DOM Lesson/index.js
@@ -1,2 +1,7 @@
 document.querySelector("h1").innerHTML = "Welcome to my website!";
-document.querySelector("ul").lastElementChild.innerHTML="cenk"
+document.querySelector("ul").lastElementChild.innerHTML="cenk";
+document.getElementsByTagName("li")[2].style.color = "red";
+document.getElementById("title").innerHTML= "Changed by ID";
+document.querySelector("#title").innerHTML = "Good Bye!"
+document.querySelector(".btn").style.color = "green";
+document.querySelector("li a").innerHTML = "Google It!";
