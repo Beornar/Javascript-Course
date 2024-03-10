e3cb42c87ecd9ecc134fab67eff6bdd9abd55f01
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-03-10 11:33:38 +0300

    async

--- sync-async/async.js
+++ sync-async/async.js
@@ -13,39 +13,41 @@
 // request.open("GET", "https://jsonplaceholder.typicode.com/comments/1");
 // request.send();
 
-const jspURI = "https://jsonplaceholder.typicode.com";
+// const jspURI = "https://jsonplaceholder.typicode.com";
 
-const getRequest = (endpoint, callbackFunction) => {
-    let result = null;
-    const request = new XMLHttpRequest();
+// const getRequest = (endpoint, callbackFunction) => {
+//     let result = null;
+//     const request = new XMLHttpRequest();
 
-    request.addEventListener("readystatechange", () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            // callbackFunction(null, data);
-            result = data;
-        }
-        else if (request.readyState === 4) {
-            // callbackFunction(`Error occured: ${request.status} - ${request.statusText}`);
-            result = `Error occured: ${request.status} - ${request.statusText}`;
+//     request.addEventListener("readystatechange", () => {
+//         if (request.readyState === 4 && request.status === 200) {
+//             const data = JSON.parse(request.responseText);
+//             // callbackFunction(null, data);
+//             result = data;
+//         }
+//         else if (request.readyState === 4) {
+//             // callbackFunction(`Error occured: ${request.status} - ${request.statusText}`);
+//             result = `Error occured: ${request.status} - ${request.statusText}`;
 
-        }
-    });
+//         }
+//     });
 
-    request.open("GET", endpoint, false);
-    request.send();
-    return result;
-}
+//     request.open("GET", endpoint, false);
+//     request.send();
+//     return result;
+// }
 
-console.log(1);
+// console.log(1);
 
-console.table(getRequest(`${jspURI}/users`));
+// console.table(getRequest(`${jspURI}/users`));
 
-console.log(2);
+// console.log(2);
 
-console.table(getRequest(`${jspURI}/todos?_limit=5`));
+// console.table(getRequest(`${jspURI}/todos?_limit=5`));
 
-console.log(3);
+// console.log(3);
+
+
 
 // console.log("Hello World!!!");
 
@@ -69,3 +71,36 @@ console.log(3);
 // });
 
 // console.log("Hello World!!!");
+
+const jspURI = "https://jsonplaceholder.typicode.com";
+
+const getPromise = (endpoint) => {
+
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener("readystatechange", () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            }
+            else if (request.readyState === 4) {
+                reject(`Error occured: ${request.status} - ${request.statusText}`);
+
+            }
+        });
+
+        request.open("GET", endpoint);
+        request.send();
+    })
+}
+
+getPromise(`${jspURI}/users/1`).then(data => {
+    console.log(data);
+    return getPromise(`${jspURI}/users/2`)
+})
+    .then(data => {
+        console.log(data);
+        return getPromise(`${jspURI}/users/3`)
+    }).then(data => console.log(data))
+    .catch(err => console.log(err));
