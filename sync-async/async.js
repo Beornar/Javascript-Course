38d61ebbb82350643190b3ec62f87e91874b3abe
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-03-11 13:40:24 +0300

    async demo

--- sync-async/async.js
+++ sync-async/async.js
@@ -72,35 +72,90 @@
 
 // console.log("Hello World!!!");
 
+
+
+/////////////////////////////////////////////////////////////
+
+// const jspURI = "https://jsonplaceholder.typicode.com";
+
+// const getPromise = (endpoint) => {
+
+//     return new Promise((resolve, reject) => {
+//         const request = new XMLHttpRequest();
+
+//         request.addEventListener("readystatechange", () => {
+//             if (request.readyState === 4 && request.status === 200) {
+//                 const data = JSON.parse(request.responseText);
+//                 resolve(data);
+//             }
+//             else if (request.readyState === 4) {
+//                 reject(`Error occured: ${request.status} - ${request.statusText}`);
+
+//             }
+//         });
+
+//         request.open("GET", endpoint);
+//         request.send();
+//     })
+// }
+
+// getPromise(`${jspURI}/users/1`).then(data => {
+//     console.log(data);
+//     return getPromise(`${jspURI}/users/2`)
+// })
+//     .then(data => {
+//         console.log(data);
+//         return getPromise(`${jspURI}/users/3`)
+//     }).then(data => console.log(data))
+//     .catch(err => console.log(err))
+//     .finally(() => console.log("All Done"));
+
+
+/////////////////////////////////////////////////////////////
+
+
+
 const jspURI = "https://jsonplaceholder.typicode.com";
 
-const getPromise = (endpoint) => {
+const newToDo = {
+    userId: 2,
+    title: "Cenk Kaynak",
+    completed: true
+};
 
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
+const updateToDo = {
+    completed: true,
+    title: "Changed the title!!!"
+};
 
-        request.addEventListener("readystatechange", () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data);
-            }
-            else if (request.readyState === 4) {
-                reject(`Error occured: ${request.status} - ${request.statusText}`);
+fetch(`${jspURI}/todos/`).then((response) => response.json()).then((json) => console.log(json));
 
-            }
-        });
-
-        request.open("GET", endpoint);
-        request.send();
-    })
-}
-
-getPromise(`${jspURI}/users/1`).then(data => {
-    console.log(data);
-    return getPromise(`${jspURI}/users/2`)
+fetch(`${jspURI}/todos/`, {
+    method: "POST",
+    body: JSON.stringify(newToDo)
 })
-    .then(data => {
-        console.log(data);
-        return getPromise(`${jspURI}/users/3`)
-    }).then(data => console.log(data))
-    .catch(err => console.log(err));
+    .then(response => {
+        if (response.ok && response.status === 201) {
+            return response.json();
+        }
+        else {
+            console.log(`Error occured: ${response.status}`);
+        }
+    })
+    .then(data => console.log(data));
+
+fetch(`${jspURI}/todos/201`, {
+    method: "DELETE",
+})
+    .then(data => console.log(data));
+
+fetch(`${jspURI}/todos?userId=2`)
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+
+fetch(`${jspURI}/todos/1`, {
+    method: 'PUT',
+    body: JSON.stringify(updateToDo)
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
