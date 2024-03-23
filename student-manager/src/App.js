6809ba3af1905c881f8a799f564265ce57e4f079
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-03-23 11:55:59 +0300

    first react project

--- student-manager/src/App.js
+++ student-manager/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [studentName1, setStudentName1] = useState("Mert Kırant");
+  const [studentName2, setStudentName2] = useState("Onur Ege Erkek");
+  const [studentName3, setStudentName3] = useState("Ali Rıza Taşkıran");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <h1>Student Manager</h1>
+
+      <p>{studentName1}</p>
+      <button onClick={() => setStudentName1("Cenk Kaynak")}>Change Student Name</button>
+
+      <p>{studentName2}</p>
+      <button onClick={() => setStudentName2("Barkın Sayın")}>Change Student Name</button>
+
+      <p>{studentName3}</p>
+      <button onClick={() => setStudentName3("Berkay Turna")}>Change Student Name</button>
+    </div >
   );
 }
 
