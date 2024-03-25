c9a4c280a9b003e09c6371e4e41030899bbccd3c
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-03-25 08:27:40 +0300

    react update

--- student-manager/src/App.js
+++ student-manager/src/App.js
@@ -11,35 +11,47 @@ function App() {
   const [courseInput, setCourseInput] = useState("");
   const [instructorInput, setInstructorInput] = useState("");
 
-  const [studentNameError, setStudentNameError] = useState("");
-  const [courseError, setCourseError] = useState("");
-  const [instructorError, setInstructorError] = useState("");
+  const [studentNameError, setStudentNameError] = useState(false);
+  const [courseError, setCourseError] = useState(false);
+  const [instructorError, setInstructorError] = useState(false);
 
   return (
     <div className="app">
       <h1>Student Manager</h1>
 
       <p>Enter Student</p>
-      <form action="">
+      <form className="student-form" action="">
         <input type="text" id="studentNameId" placeholder='Student Name' value={studentNameInput} onChange={(event) => setStudentNameInput(event.target.value)} />
-        <p>{studentNameError}</p>
+        <br />
+        {studentNameError && <p>Please enter your name</p>}
         <input type="text" id="courseId" placeholder='Course' value={courseInput} onChange={(event) => setCourseInput(event.target.value)} />
-        <p>{courseError}</p>
+        {courseError && <p>Please enter course name</p>}
+        <br />
         <input type="text" id="instructorId" placeholder='Instructor' value={instructorInput} onChange={(event) => setInstructorInput(event.target.value)} />
-        <p>{instructorError}</p>
+        {instructorError && <p>Please enter your instructor's name</p>}
+        <br />
         <input type="submit" onClick={(event) => {
           event.preventDefault();
-          if (!studentNameInput || !courseInput || !instructorInput) {
-            setStudentNameError(studentNameInput.trim() ? "" : "Name can not be empty");
-            setCourseError(courseInput.trim() ? "" : "Course can not be empty");
-            setInstructorError(instructorInput.trim() ? "" : "Instructor can not be empty");
+
+          setStudentNameError(false);
+          setCourseError(false);
+          setInstructorError(false);
+
+          if (!studentNameInput.trim() || !courseInput.trim() || !instructorInput.trim()) {
+            // setStudentNameError(studentNameInput.trim() ? "" : "Please enter your name");
+            // setCourseError(courseInput.trim() ? "" : "Please enter course");
+            // setInstructorError(instructorInput.trim() ? "" : "Please enter instructor name");
+            !studentNameInput.trim() && setStudentNameError(true);
+            !courseInput.trim() && setCourseError(true);
+            !instructorInput.trim() && setInstructorError(true);
+          } else {
+            setStudentName(studentNameInput.trim());
+            setCourse(courseInput.trim());
+            setInstructor(instructorInput.trim());
+            setStudentNameInput("");
+            setCourseInput("");
+            setInstructorInput("");
           }
-          setStudentName(studentNameInput.trim());
-          setCourse(courseInput.trim());
-          setInstructor(instructorInput.trim());
-          setStudentNameInput("");
-          setCourseInput("");
-          setInstructorInput("");
         }} />
       </form>
       <br />
