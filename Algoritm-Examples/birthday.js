3404a46adff18611682143de5bc4209d36f67858
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-02 11:49:11 +0300

    algo update

--- Algoritm-Examples/birthday.js
+++ Algoritm-Examples/birthday.js
@@ -6,3 +6,16 @@ birthday.push([20, 09, 1991]);
 birthday.push([23, 03, 2004]);
 birthday.push([13, 12, 2010]);
 birthday.push([15, 05, 2019]);
+
+function calculateAge(birth, date) {
+    const [day, month, year] = birth;
+    const [currentDay, currentMonth, currentYear] = date;
+    let age = currentYear - year;
+    return age;
+}
+
+birthday.forEach(birthday => {
+    const [day, month, year] = birthday;
+    const age = calculateAge(birthday,today);
+    console.log(`${day} ${month} ${year} doğumlu kişi ${age} yaşındadır`);
+})
