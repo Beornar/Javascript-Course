7807230d121124817d076647470820c41452703c
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-02 11:36:16 +0300

    algorithm update

---
+++ Algoritm-Examples/birthday.js
@@ -0,0 +1,8 @@
+const birthday = [[11, 11, 1985], [25, 11, 1999], [08, 04, 1992], [30, 04, 1978]];
+const today = [30, 04, 2024];
+
+birthday.push([27, 03, 1991]);
+birthday.push([20, 09, 1991]);
+birthday.push([23, 03, 2004]);
+birthday.push([13, 12, 2010]);
+birthday.push([15, 05, 2019]);
