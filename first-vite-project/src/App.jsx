a67117428fcad0cf39c5d560a1771f532b1702df
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-04 10:10:31 +0300

    adding components

--- first-vite-project/src/App.jsx
+++ first-vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/shared/Header.jsx'
 
 function App() {
 
@@ -40,6 +41,7 @@ function App() {
 
   return (
     <main>
+      <Header/>
       <form action="" className='student-form'>
         <div className='input-control'>
           <input type="text" placeholder='Student Name'
