1d3efadf408cc46cd7e12aa77ef63a7e37519d90
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-06 11:25:41 +0300

    update

--- first-vite-project/src/App.jsx
+++ first-vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/shared/header/Header.jsx'
 import StudentList from './app/student/student-list/StudentList.jsx'
@@ -6,20 +6,14 @@ import StudentForm from './app/student/student-form/StudentForm.jsx'
 
 function App() {
 
-  const [studentList, setStudentList] = useState([
-    {
-      studentName: "Barkın Onay Sayın",
-      course: "React",
-      instructor: "Hicran Ertuğral",
-      id: "001",
-    },
-    {
-      studentName: "Onur Ege Erkallbek",
-      course: "JavaScript",
-      instructor: "Buse Uğraş",
-      id: "002",
-    },
-  ]);
+  const [studentList, setStudentList] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5050/students`)
+      .then(response => response.json())
+      .then(data => setStudentList(data))
+  }, []);
+
 
   const addStudent = (student) => {
     setStudentList(prevStudentList => [...prevStudentList, { ...student, id: Date.now().toString() }])
