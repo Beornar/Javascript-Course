8a6750f08d2f89ec500d93b8f2eea02412d1faa4
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-06 12:53:25 +0300

    javascript tests

---
+++ Testing Pages/test.js
@@ -0,0 +1,36 @@
+// let tweet = prompt("Enter your tweet");
+// let tweetLenght = tweet.length;
+// console.log("Your tweet has " + tweetLenght + " characters. You can type " + (140 - tweetLenght) + " characters more.");
+
+// if (tweetLenght>140){
+//     let newTweet = tweet.slice(0,140);
+//     console.log("Your tweet has more than 140 characters. So here is your new tweet:\n" + newTweet);
+// }else {
+//     console.log(tweet);
+// }
+
+// let askName = prompt("Enter your name:");
+// let firstChar = askName.slice(0,1).toUpperCase();
+// let restOfName = askName.slice(1).toLocaleLowerCase();
+// console.log(firstChar+restOfName);
+
+function getMilk() {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
