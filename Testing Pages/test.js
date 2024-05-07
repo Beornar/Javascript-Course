3dd649df63ef541adfa6d94b3cd20aec025a9dee
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-07 13:14:16 +0300

    javascript test

--- Testing Pages/test.js
+++ Testing Pages/test.js
@@ -13,24 +13,3 @@
 // let firstChar = askName.slice(0,1).toUpperCase();
 // let restOfName = askName.slice(1).toLocaleLowerCase();
 // console.log(firstChar+restOfName);
-
-function getMilk() {
-    console.log("leaveHouse");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveUp");
-    console.log("moveRight");
-    console.log("moveRight");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("moveLeft");
-    console.log("enterHouse");
-}
